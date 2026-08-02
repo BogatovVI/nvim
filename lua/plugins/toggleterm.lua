9949713850ce2c63f609814cb0c5157833bdee7f
BogatovVI <vladosdydos@mail.ru>
2026-08-02 22:57:14 +0300

    Исправил опечатку

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,45 +1,44 @@
 return {
-  {
-    "akinsho/toggleterm.nvim",
-    version = "*",
-    keys = {
-      { [[<C-\>]], desc = "Переключить терминал (Ctrl + \\)" },Ж
-    },
-    config = function()
-      require("toggleterm").setup({
-        size = 15,
-        open_mapping = [[<C-\>]],
-        hide_numbers = true,
-        direction = "float",
-        close_on_exit = true,
-        
-        float_opts = {
-          border = "rounded", 
-          winblend = 3,       
-          highlights = {
-            border = "Normal",
-            background = "Normal",
-          },
-        },
-      })
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		keys = {
+			{ [[<C-\>]], desc = "Переключить терминал (Ctrl + \\)" },
+		},
+		config = function()
+			require("toggleterm").setup({
+				size = 15,
+				open_mapping = [[<C-\>]],
+				hide_numbers = true,
+				direction = "float",
+				close_on_exit = true,
 
-      -- Специальные настройки навигации внутри самого терминала
-      function _G.set_terminal_keymaps()
-        local opts = { buffer = 0 }
-        vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], opts)
-        
-        vim.keymap.set('t', '<C-h>', [[<C-\><C-n><C-w>h]], opts)
-        vim.keymap.set('t', '<C-j>', [[<C-\><C-n><C-w>j]], opts)
-        vim.keymap.set('t', '<C-k>', [[<C-\><C-n><C-w>k]], opts)
-        vim.keymap.set('t', '<C-l>', [[<C-\><C-n><C-w>l]], opts)
-      end
+				float_opts = {
+					border = "rounded",
+					winblend = 3,
+					highlights = {
+						border = "Normal",
+						background = "Normal",
+					},
+				},
+			})
 
-      vim.api.nvim_create_autocmd("TermOpen", {
-        pattern = "term://*",
-        callback = function()
-          _G.set_terminal_keymaps()
-        end,
-      })
-    end,
-  },
+			-- Специальные настройки навигации внутри самого терминала
+			function _G.set_terminal_keymaps()
+				local opts = { buffer = 0 }
+				vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], opts)
+				vim.keymap.set("t", "<C-h>", [[<C-\><C-n><C-w>h]], opts)
+				vim.keymap.set("t", "<C-j>", [[<C-\><C-n><C-w>j]], opts)
+				vim.keymap.set("t", "<C-k>", [[<C-\><C-n><C-w>k]], opts)
+				vim.keymap.set("t", "<C-l>", [[<C-\><C-n><C-w>l]], opts)
+			end
+
+			vim.api.nvim_create_autocmd("TermOpen", {
+				pattern = "term://*",
+				callback = function()
+					_G.set_terminal_keymaps()
+				end,
+			})
+		end,
+	},
 }
